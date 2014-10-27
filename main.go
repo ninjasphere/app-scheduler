e10a0b62a17d99b8fcd6b5bbbb0564cb650e7642
Jon Seymour <jon.seymour@gmail.com>
2014-10-27 17:22:28 +1100

    Export the application onto the bus.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-       "github.com/ninjasphere/go-ninja/support"
+	"github.com/ninjasphere/go-ninja/api"
+	"github.com/ninjasphere/go-ninja/support"
 )
 
+var (
+	info = ninja.LoadModuleInfo("./package.json")
+)
+
+type service struct {
+}
+
+type SchedulerApp struct {
+	support.AppSupport
+}
+
+type Config struct {
+}
+
+func (a *SchedulerApp) Start(config *Config) error {
+	a.SendEvent("config", config)
+	return nil
+}
+
+func (a *SchedulerApp) Stop() error {
+	return nil
+}
+
 func main() {
-     support.WaitUntilSignal()
+	app := &SchedulerApp{}
+	err := app.Init(info)
+	if err != nil {
+		app.Log.Fatalf("failed to initialize app: %v", err)
+	}
+
+	err = app.Export(app)
+	if err != nil {
+		app.Log.Fatalf("failed to export app: %v", err)
+	}
+
+	support.WaitUntilSignal()
 }
