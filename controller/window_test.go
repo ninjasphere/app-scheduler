d5e65707d95733993f0a9965cc3996ea3b7923d2
Jon Seymour <jon@ninjablocks.com>
2014-10-29 16:59:48 +1100

    Add tests for non-recurring windows.

--- controller/window_test.go
+++ controller/window_test.go
@@ -35,26 +35,56 @@ func TestBadInitUntilNil(t *testing.T) {
 	}
 }
 
-func TestEarlierWindowIsNotOpen(t *testing.T) {
+func runNonOverlappingWindow(t *testing.T, m *model.Window, permanentlyClosed bool) {
 	initMockClock(testTime)
 	stub := &window{}
-	err := stub.init(earlierWindow)
+	err := stub.init(m)
 	if err != nil {
 		t.Fatalf("unexpected error while opening window: %v", err)
 	}
 	if stub.isOpen(testTime) {
 		t.Fatalf("window should not be open now")
 	}
+	if permanentlyClosed != stub.isPermanentlyClosed(testTime) {
+		t.Fatalf("expecting permanentlyClosed == %v, but found opposite", permanentlyClosed)
+	}
 }
 
-func TestOverlappingWindowIsNotOpen(t *testing.T) {
+func runOverlappingWindow(t *testing.T, m *model.Window) {
 	initMockClock(testTime)
 	stub := &window{}
-	err := stub.init(overlappingWindow)
+	err := stub.init(m)
 	if err != nil {
 		t.Fatalf("unexpected error while opening window: %v", err)
 	}
 	if !stub.isOpen(testTime) {
 		t.Fatalf("window should be open now")
 	}
+	if stub.isPermanentlyClosed(testTime) {
+		t.Fatalf("expecting permanentlyClosed == false, but found opposite")
+	}
+}
+
+func TestEarlierTimeOfDayWindowIsNotOpen(t *testing.T) {
+	runNonOverlappingWindow(t, earlierTimeOfDayWindow, false)
+}
+
+func TestLaterTimeOfDayWindowIsNotOpen(t *testing.T) {
+	runNonOverlappingWindow(t, laterTimeOfDayWindow, false)
+}
+
+func TestOverlappingTimeOfDayWindowIsNotOpen(t *testing.T) {
+	runOverlappingWindow(t, overlappingTimeOfDayWindow)
+}
+
+func TestEarlierTimestampWindowIsNotOpen(t *testing.T) {
+	runNonOverlappingWindow(t, earlierTimestampWindow, true)
+}
+
+func TestLaterTimestampWindowIsNotOpen(t *testing.T) {
+	runNonOverlappingWindow(t, laterTimestampWindow, false)
+}
+
+func TestOverlappingTimestampWindowIsNotOpen(t *testing.T) {
+	runOverlappingWindow(t, overlappingTimestampWindow)
 }
