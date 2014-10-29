1aecf83292dd1e385df30cd8c5c11aab08c1737c
Jon Seymour <jon.seymour@gmail.com>
2014-10-29 11:11:02 +1100

    Introduce a mock clock to allow simulation of scheduler.

---
+++ controller/mockclock.go
@@ -0,0 +1,52 @@
+package controller
+
+import (
+	"time"
+)
+
+type mockclock struct {
+	now       time.Time
+	callbacks map[time.Time][]callback
+}
+
+func initMockClock(now time.Time) *mockclock {
+	mock := &mockclock{
+		now:       now,
+		callbacks: make(map[time.Time][]callback),
+	}
+	clock = mock
+	return mock
+}
+
+func (m *mockclock) Now() time.Time {
+	return m.now
+}
+
+func (m *mockclock) SetNow(now time.Time) {
+	m.now = now
+	saved := make(map[time.Time][]callback)
+	for t, cbs := range m.callbacks {
+		if t.Sub(now) >= 0 {
+			for _, cb := range cbs {
+				cb()
+			}
+		} else {
+			saved[t] = cbs
+		}
+	}
+	m.callbacks = saved
+}
+
+func (m *mockclock) AfterFunc(delay time.Duration, then func()) {
+	if delay <= 0 {
+		then()
+	} else {
+		when := m.now.Add(delay)
+		var list []callback
+		if list, ok := m.callbacks[when]; !ok {
+			list = make([]callback, 0)
+			m.callbacks[when] = list
+		}
+		m.callbacks[when] = append(list, then)
+	}
+}
