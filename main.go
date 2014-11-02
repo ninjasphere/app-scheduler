eef73a86e4bc6d5eb59c9221473b72a3d821d7f0
Jon Seymour <jon@ninjablocks.com>
2014-11-02 19:07:33 +1100

    Split main.go into service.go

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/ninjasphere/app-scheduler/controller"
 	"github.com/ninjasphere/app-scheduler/model"
 	"github.com/ninjasphere/go-ninja/api"
-	"github.com/ninjasphere/go-ninja/config"
-	"github.com/ninjasphere/go-ninja/logger"
-	nmodel "github.com/ninjasphere/go-ninja/model"
-	"github.com/ninjasphere/go-ninja/rpc"
 	"github.com/ninjasphere/go-ninja/support"
-	"time"
 )
 
 var (
@@ -49,80 +43,6 @@ func (a *SchedulerApp) Stop() error {
 		err = tmp.scheduler.Stop()
 	}
 	return err
-}
-
-//SchedulerService is a facade used to prevent the app event handler being replaced
-//by the service event handler.
-type SchedulerService struct {
-	scheduler   *controller.Scheduler
-	log         *logger.Logger
-	conn        *ninja.Connection
-	model       *model.Schedule
-	service     *rpc.ExportedService
-	configStore func(m *model.Schedule)
-}
-
-func (s *SchedulerService) init(moduleID string) error {
-	s.scheduler = &controller.Scheduler{}
-	s.scheduler.SetLogger(s.log)
-	s.scheduler.SetConnection(s.conn, time.Millisecond*time.Duration(config.Int(10000, "scheduler", "timeout")))
-	if err := s.scheduler.Start(s.model); err != nil {
-		return err
-	}
-
-	var err error
-	topic := fmt.Sprintf("$node/%s/app/%s/service/%s", config.Serial(), moduleID, "scheduler")
-	announcement := &nmodel.ServiceAnnouncement{
-		Schema: "http://schema.ninjablocks.com/service/scheduler",
-	}
-	if s.service, err = s.conn.ExportService(s, topic, announcement); err != nil {
-		return err
-	}
-	s.configStore(s.model)
-	return nil
-}
-
-// Schedule a new task or re-schedules an existing task.
-func (s *SchedulerService) Schedule(task *model.Task) (*string, error) {
-	if s.scheduler != nil {
-		err := s.Cancel(task.ID)
-		if err != nil {
-			s.log.Warningf("cancel failed %s", err)
-		}
-		err = s.scheduler.Schedule(task)
-		if err == nil {
-			s.model.Tasks = append(s.model.Tasks, task)
-			s.configStore(s.model)
-		}
-		copy := task.ID
-		return &copy, err
-	}
-	return nil, fmt.Errorf("cannot schedule a task while the scheduler is stopped")
-}
-
-// Cancel an existing task.
-func (s *SchedulerService) Cancel(taskID string) error {
-	if s.scheduler != nil {
-		var err error
-		found := -1
-		for i, t := range s.model.Tasks {
-			if t.ID == taskID {
-				found = i
-				break
-			}
-		}
-		if found > -1 {
-			err = s.scheduler.Cancel(taskID)
-			if err == nil {
-				s.model.Tasks = append(s.model.Tasks[0:found], s.model.Tasks[found+1:]...)
-				s.configStore(s.model)
-			}
-		} else {
-			err = nil
-		}
-		return err
-	}
-	return fmt.Errorf("cannot cancel a task while the scheduler is stopped")
 }
 
 func main() {
