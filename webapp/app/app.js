30fd5bacbda47b285c5cd7232171d73df93815a6
Jon Seymour <jon@ninjablocks.com>
2015-02-03 17:03:25 +1100

    Add wireframe for list and edit views.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -1,12 +1,48 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+angular.module('schedulerApp', [
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.when('/list', {templateUrl: 'task-list.html', controller: 'TaskList'});
+  $routeProvider.when('/edit/:task', {templateUrl: 'task-edit.html', controller: 'TaskEdit'});
+  $routeProvider.when('/create', {templateUrl: 'task-edit.html', controller: 'TaskEdit'});
+  $routeProvider.otherwise({redirectTo: '/list'});
+}])
+.controller('TaskList', ['$scope', function($scope) {
+	$scope.tasks = [ {
+			"id": "1",
+			"description": "task 1"
+	}]
+}])
+.controller('TaskEdit', ['$scope', '$location', function($scope, $location) {
+	$scope.description = "schedule item"
+	$scope.timeOfDay = "09:30:00"
+	$scope.selectedThings = [
+		{
+			"description": "Downlight",
+			"room": "Lounge",
+			"on": true,
+		}
+	]
+
+	$scope.availableThings = [
+		{
+			"description": "Lamp",
+			"room": "Bedroom"
+		}
+	]
+
+	$scope.save = function() {
+
+	}
+
+	$scope.delete = function() {
+
+	}
+
+	$scope.cancel = function() {
+		$location.path('/list')
+	}
+}])
