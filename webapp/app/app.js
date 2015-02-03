e322dcfee9af50152f18b63f8b24a76965b06a5e
Jon Seymour <jon@ninjablocks.com>
2015-02-04 10:16:53 +1100

    wip

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -3,8 +3,24 @@
 // Declare app level module which depends on views, and components
 angular.module('schedulerApp', [
   'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+.factory('scheduler', [ '$resource', function($resource) {
+
+	var
+		Things = $resource("http://10.0.1.168:8000/rest/v1/things", {})
+		service = {
+			refresh: function() {
+				return $resource.get({}).$promise.then(
+					function(things) {
+						return things
+					}
+				).$promise
+			}
+
+		}
+	return service
+}])
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/list', {templateUrl: 'task-list.html', controller: 'TaskList'});
   $routeProvider.when('/edit/:task', {templateUrl: 'task-edit.html', controller: 'TaskEdit'});
   $routeProvider.when('/create', {templateUrl: 'task-edit.html', controller: 'TaskEdit'});
@@ -16,7 +32,7 @@ config(['$routeProvider', function($routeProvider) {
 			"description": "task 1"
 	}]
 }])
-.controller('TaskEdit', ['$scope', '$location', function($scope, $location) {
+.controller('TaskEdit', ['$scope', '$location', 'scheduler', function($scope, $location, scheduler) {
 	$scope.description = "schedule item"
 	$scope.timeOfDay = "09:30:00"
 	$scope.selectedThings = [
@@ -33,6 +49,15 @@ config(['$routeProvider', function($routeProvider) {
 			"room": "Bedroom"
 		}
 	]
+
+	scheduler.refresh().done(
+		function(things) {
+			console.debug(things)
+		},
+		function(err) {
+			$scope.message = err
+		}
+	)
 
 	$scope.save = function() {
 
