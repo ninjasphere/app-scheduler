d7a3f55eecf6b5e58fa40a414b9a15ce53f8fa7f
Jon Seymour <jon@ninjablocks.com>
2015-02-07 14:49:43 +1100

    Add mock data.

--- webapp/app/services/mocks/rooms.js
+++ webapp/app/services/mocks/rooms.js
@@ -1,1 +1,12 @@
-{}
+{
+  "f0b4a3ac-a818-11e4-8ab9-7c669d02a706": {
+    "name": "Kitchen",
+    "id": "f0b4a3ac-a818-11e4-8ab9-7c669d02a706",
+    "type": "kitchen"
+  },
+  "9e17dc6f-a813-11e4-8ab9-7c669d02a706": {
+    "name": "Lounge",
+    "id": "9e17dc6f-a813-11e4-8ab9-7c669d02a706",
+    "type": "lounge"
+  }
+}
